25b70f0eb698e275783667a31c070927d80ad470
Oitash <oitashg@gmail.com>
2024-07-17 02:26:53 +0530

    Cycle detection in directed graph using DFS

--- Graph/cycleDetection.cpp
+++ Graph/cycleDetection.cpp
@@ -1,5 +1,3 @@
-//-------------------------------Undirected Graph-------------------------------------
-
 #include <iostream>
 #include <vector>
 #include <queue>
@@ -7,85 +5,151 @@
 #include <list>
 using namespace std;
 
-class Solution {
-  public:
-    //--------------Detect cycle using BFS-----------------------------
+class Solution
+{
+public:
+    //-------------------------------Undirected Graph-------------------------------------
 
-    bool detectCycleBfs(int src, vector<int> adj[], vector<int>& vis){
+    //------------------Detect cycle using BFS-----------------------------
+
+    bool detectCycleBfs(int src, vector<int> adj[], vector<int> &vis)
+    {
         vis[src] = 1;
-        queue<pair<int,int>> q;
-        q.push({src,-1});
-        
-        while(!q.empty()){
+        queue<pair<int, int>> q;
+        q.push({src, -1});
+
+        while (!q.empty())
+        {
             int node = q.front().first;
             int parent = q.front().second;
             q.pop();
-            
-            for(auto it: adj[node]){
-                if(!vis[it]){
+
+            for (auto it : adj[node])
+            {
+                if (!vis[it])
+                {
                     vis[it] = 1;
-                    q.push({it,node});
+                    q.push({it, node});
                 }
-                else if(parent != it)
+                else if (parent != it)
                     return true;
             }
         }
-        
+
         return false;
     }
 
-    bool isCycle(int V, vector<int> adj[]) {
-        vector<int> vis(V,0);
-        
-        for(int i=0; i<V; i++){
-            if(!vis[i]){
-                if(detectCycleBfs(i, adj, vis)) return true;
+    bool isCycle(int V, vector<int> adj[])
+    {
+        vector<int> vis(V, 0);
+
+        for (int i = 0; i < V; i++)
+        {
+            if (!vis[i])
+            {
+                if (detectCycleBfs(i, adj, vis))
+                    return true;
             }
         }
-        
+
         return false;
     }
 
     //-----------------------Detect cycle using DFS-----------------------------
 
-    bool detectCycleDfs(int src, int parent, vector<int> adj[], vector<int>& vis){
+    bool detectCycleDfs(int src, int parent, vector<int> adj[], vector<int> &vis)
+    {
         vis[src] = 1;
-        
-        for(auto it: adj[src]){
-            if(!vis[it]){
+
+        for (auto it : adj[src])
+        {
+            if (!vis[it])
+            {
                 bool res = detectCycleDfs(it, src, adj, vis);
-                if(res) return true;
+                if (res)
+                    return true;
             }
-            else if(it != parent)  return true;
+            else if (it != parent)
+                return true;
         }
-        
-        return false;
-    }
-    
-    bool isCycle(int V, vector<int> adj[]) {
-        vector<int> vis(V,0);
-        
-        for(int i=0; i<V; i++){
-            if(!vis[i]){
-                if(detectCycleDfs(i, -1, adj, vis)) return true;
-            }
-        }
-        
+
         return false;
     }
 
-    //------------------------------------------------------------------
+    bool isCycle(int V, vector<int> adj[])
+    {
+        vector<int> vis(V, 0);
+
+        for (int i = 0; i < V; i++)
+        {
+            if (!vis[i])
+            {
+                if (detectCycleDfs(i, -1, adj, vis))
+                    return true;
+            }
+        }
+
+        return false;
+    }
+
+    //-------------------------------------------------------------------------
+
+    //-------------------------------Directed Graph-----------------------------------------
+
+    //-----------------------Detect cycle using DFS-----------------------------
+
+    bool dfs(int node, vector<int> &vis, vector<int> &pathVis, vector<int> adj[])
+    {
+        vis[node] = 1;
+        pathVis[node] = 1;
+
+        for (auto it : adj[node])
+        {
+            if (!vis[it])
+            {
+                bool res = dfs(it, vis, pathVis, adj);
+                if (res)
+                    return true;
+            }
+            else if (pathVis[it])
+                return true;
+        }
+
+        pathVis[node] = 0;
+        return false;
+    }
+
+    bool isCyclic(int V, vector<int> adj[])
+    {
+        vector<int> vis(V, 0), pathVis(V, 0);
+
+        for (int i = 0; i < V; i++)
+        {
+            if (!vis[i])
+            {
+                bool ans = dfs(i, vis, pathVis, adj);
+                if (ans)
+                    return true;
+            }
+        }
+
+        return false;
+    }
+
+    //----------------------------------------------------------------
 };
 
-
-int main() {
+int main()
+{
     int tc;
     cin >> tc;
-    while (tc--) {
+    while (tc--)
+    {
         int V, E;
         cin >> V >> E;
         vector<int> adj[1000];
-        for (int i = 0; i < E; i++) {
+        for (int i = 0; i < E; i++)
+        {
             int u, v;
             cin >> u >> v;
             adj[u].push_back(v);
@@ -100,6 +164,3 @@ int main() {
     }
     return 0;
 }
-
-//-------------------------------Directed Graph-----------------------------------------
-
