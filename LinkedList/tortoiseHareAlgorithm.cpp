f856e1fae47223cd20e30181549a7424704431a0
Oitash <oitashg@gmail.com>
2024-07-25 13:26:05 +0530

    Some questions updated

--- LinkedList/tortoiseHareAlgorithm.cpp
+++ LinkedList/tortoiseHareAlgorithm.cpp
@@ -13,7 +13,8 @@ struct ListNode
 class Solution
 {
 public:
-    //----------Tortoise-Hare Algorithm--------------
+    //----------------Tortoise-Hare Algorithm-----------------
+    //-------------------Find middle of LL--------------------
 
     ListNode *middleNode(ListNode *head)
     {
@@ -22,16 +23,68 @@ public:
 
         ListNode *slow = head;
         ListNode *fast = head;
-        
-        //when length of ll is odd, fast will end that last node and for even length it will end at null
+
+        // when length of ll is odd, fast will end that last node and for even length it will end at null
         while (fast != NULL && fast->next != NULL)
         {
             slow = slow->next;
             fast = fast->next->next;
         }
-        
+
         return slow;
     }
 
-    //---------------------------------------------------
+    //------------------Detect cycle in LL----------------------------
+
+    bool hasCycle(ListNode *head)
+    {
+        ListNode *slow = head;
+        ListNode *fast = head;
+
+        while (fast != NULL && fast->next != NULL)
+        {
+            slow = slow->next;
+            fast = fast->next->next;
+
+            if (slow == fast)
+                return true;
+        }
+
+        return false;
+    }
+
+    //---------------------Find length of loop in LL------------------------------------
+
+    int findLength(ListNode *slow, ListNode *fast)
+    {
+        int cnt = 1;
+        fast = fast->next;
+
+        while (slow != fast)
+        {
+            cnt++;
+            fast = fast->next;
+        }
+
+        return cnt;
+    }
+
+    int lengthOfLoop(ListNode *head)
+    {
+        ListNode *slow = head;
+        ListNode *fast = head;
+
+        while (fast != NULL && fast->next != NULL)
+        {
+            slow = slow->next;
+            fast = fast->next->next;
+
+            if (slow == fast)
+                return findLength(slow, fast);
+        }
+
+        return 0;
+    }
+
+    //----------------------------------------------------------------
 };
