e204bdfe94f808f2dfea0967a79b49a622fa4395
Oitash <oitashg@gmail.com>
2024-08-03 09:06:04 +0530

    Symmetric tree

---
+++ Binary Tree/symmetricTree.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <map>
+using namespace std;
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    bool f(TreeNode* left, TreeNode* right){
+        if(left == NULL || right == NULL)
+            return left == right;
+
+        if(left->val != right->val) return false;
+
+        bool ans = f(left->left, right->right) && f(left->right, right->left);
+        return ans;
+    }
+
+    bool isSymmetric(TreeNode* root) {
+        if(root == NULL) return true;
+
+        return f(root->left, root->right);
+    }
+};
