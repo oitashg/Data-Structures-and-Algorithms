15ab6a568ff0db296aa269883edfb33eed202261
Oitash <oitashg@gmail.com>
2024-08-25 21:50:15 +0530

    Kadane's Algorithm

---
+++ Arrays/KadaneAlgorithm.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    //---------Kadanes's Algorithm------------------
+
+    int maxSubArray(vector<int>& nums) {
+        int n = nums.size();
+        int sum = 0, ans = INT_MIN;
+
+        for(int i=0; i<n; i++){
+            //adding elements in the window
+            sum += nums[i];
+            //if sum > ans---> update ans
+            ans = max(ans, sum);
+            //if current window sum comes negative, we will reinitialize sum to 0 as we want maximum sum
+            if(sum < 0) sum = 0;
+        }
+
+        return ans;
+    }
+
+    //----------------------------------------------
+};
